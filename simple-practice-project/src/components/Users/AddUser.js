cd4e9862b0df1207b2b55ab6af7eb261929660cc
Tinsae <tigial3535@gmail.com>
2021-06-18 19:30:07 -0500

    fragments, portals & refs

--- simple-practice-project/src/components/Users/AddUser.js
+++ simple-practice-project/src/components/Users/AddUser.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
 
 import Card from '../UI/Card'
 import Button from '../UI/Button'
@@ -6,12 +6,19 @@ import ErrorModal from '../UI/ErrorModal';
 import classes from './AddUser.module.css'
 
 const AddUser = ({ onAddUser }) => {
+    /* using refs instead of state */
+    /*
     const [username, setUsername] = useState('')
     const [age, setAge] = useState('')
+    */
+    const nameRef = useRef()
+    const ageRef = useRef()
     const [error, setError] = useState();
 
     const addUserHandler = (event) => {
         event.preventDefault()
+        const username = nameRef.current.value;
+        const age = ageRef.current.value;
         if (username.trim().length === 0 || age.trim().length === 0) {
             setError({
                 title: 'Invalid input',
@@ -26,24 +33,30 @@ const AddUser = ({ onAddUser }) => {
             });
             return
         }
-        setUsername('')
-        setAge('')
+        /* using refs instead of state */
+        /* setUsername('')
+           setAge('')
+        */
         onAddUser(username, age);
+        nameRef.current.value = ''
+        ageRef.current.value = ''
     };
+
+    /* using refs instead of state */
+    /*
     const usernameChangeHandler = (event) => {
         setUsername(event.target.value)
     };
 
     const ageChangeHandler = (event) => {
         setAge(event.target.value)
-    };
+    };*/
     const errorHandler = () => {
         setError(null);
     };
 
-
     return (
-        <div>
+        <>
             {error && (
                 <ErrorModal
                     title={error.title}
@@ -54,13 +67,13 @@ const AddUser = ({ onAddUser }) => {
             <Card className={classes.input}>
                 <form onSubmit={addUserHandler}>
                     <label htmlFor="username">Username</label>
-                    <input id="username" type="text" value={username} onChange={usernameChangeHandler} />
+                    <input id="username" type="text" /*value={username} onChange={usernameChangeHandler}*/ ref={nameRef} />
                     <label htmlFor="age">Age (Years)</label>
-                    <input id="age" type="number" value={age} onChange={ageChangeHandler} />
+                    <input id="age" type="number" /*value={age} onChange={ageChangeHandler}*/ ref={ageRef} />
                     <Button type="submit">Add User</Button>
                 </form>
             </Card>
-        </div>
+        </>
     );
 };
 export default AddUser;
